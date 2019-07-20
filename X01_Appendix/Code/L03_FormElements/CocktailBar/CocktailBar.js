58d6cc1705ab34c6abcc9fdf71a6cdc35edeae2d
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2019-07-20 12:11:56 +0200

    Started code and activity diagram

---
+++ X01_Appendix/Code/L03_FormElements/CocktailBar/CocktailBar.js
@@ -0,0 +1,42 @@
+"use strict";
+var L03_CocktailBar;
+(function (L03_CocktailBar) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Init");
+        let fieldsets = document.querySelectorAll("fieldset");
+        // Install listeners on fieldsets
+        for (let i = 0; i < fieldsets.length; i++) {
+            let fieldset = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+            fieldset.addEventListener("input", handleChange);
+        }
+    }
+    function handleChange(_event) {
+        let target = _event.target;
+        console.log();
+        if (_event.type == "change")
+            console.warn("Change: " + target.name + " = " + target.value, _event);
+        else
+            console.log("Input: " + target.name + " = " + target.value, _event);
+        // Handling checkbox
+        if (target.type == "checkbox")
+            console.log("Checked: " + target.name + " = " + target.checked);
+        // Slider response
+        // if (target.name == "Slider") {
+        //     let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("progress")[0];
+        //     progress.value = parseFloat(target.value);
+        // }
+        // Meter response
+        // if (target.name == "Stepper") {
+        //     let meter: HTMLMeterElement = <HTMLMeterElement>document.querySelector("meter");
+        //     meter.value = parseFloat(target.value);
+        // }
+        // Color response
+        // if (target.name == "Color") {
+        //     let ouput: HTMLOutputElement = <HTMLOutputElement>document.querySelector("output");
+        //     ouput.value = target.value;
+        // }
+    }
+})(L03_CocktailBar || (L03_CocktailBar = {}));
+//# sourceMappingURL=CocktailBar.js.map
---
+++ X01_Appendix/Code/L03_FormElements/CocktailBar/CocktailBar.ts
@@ -0,0 +1,46 @@
+namespace L03_CocktailBar {
+    window.addEventListener("load", init);
+
+    function init(_event: Event): void {
+        console.log("Init");
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.querySelectorAll("fieldset");
+
+        // Install listeners on fieldsets
+        for (let i: number = 0; i < fieldsets.length; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+            fieldset.addEventListener("input", handleChange);
+        }
+    }
+
+    function handleChange(_event: Event): void {
+        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+        console.log();
+        if (_event.type == "change")
+            console.warn("Change: " + target.name + " = " + target.value, _event);
+        else
+            console.log("Input: " + target.name + " = " + target.value, _event);
+
+        // Handling checkbox
+        if (target.type == "checkbox")
+            console.log("Checked: " + target.name + " = " + target.checked);
+
+        // Slider response
+        // if (target.name == "Slider") {
+        //     let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("progress")[0];
+        //     progress.value = parseFloat(target.value);
+        // }
+
+        // Meter response
+        // if (target.name == "Stepper") {
+        //     let meter: HTMLMeterElement = <HTMLMeterElement>document.querySelector("meter");
+        //     meter.value = parseFloat(target.value);
+        // }
+
+        // Color response
+        // if (target.name == "Color") {
+        //     let ouput: HTMLOutputElement = <HTMLOutputElement>document.querySelector("output");
+        //     ouput.value = target.value;
+        // }
+    }
+}
