8c6f5d8b4ee464549096571747ff4b0d68d0a9c5
Jirka <del@hs-furtwangen.de>
2019-10-27 10:18:02 +0100

    L08 Code nach Dreh

--- X01_Appendix/Code/L08_Canvas/Alley/Alley.js
+++ X01_Appendix/Code/L08_Canvas/Alley/Alley.js
@@ -10,19 +10,27 @@ var L08_Canvas_Alley;
             return;
         crc2 = canvas.getContext("2d");
         let horizon = crc2.canvas.height * golden;
+        let streetWidthBack = 100;
+        let streetWidthFront = 600;
+        let treesOffsetBack = 15;
+        let treesOffsetFront = 100;
         let posMountains = { x: 0, y: horizon };
         let posStreet = { x: crc2.canvas.width / 2, y: horizon };
-        let posTreesStart = { x: crc2.canvas.width * 0.42, y: horizon };
-        let posTreesEnd = { x: 0, y: crc2.canvas.height };
+        let posTreesStart = { x: posStreet.x - streetWidthBack / 2 - treesOffsetBack, y: horizon };
+        let posTreesEnd = { x: crc2.canvas.width / 2 - streetWidthFront / 2 - treesOffsetFront, y: crc2.canvas.height };
         drawBackground();
-        drawSun({ x: 100, y: 80 });
-        drawCloud({ x: 560, y: 100 }, { x: 200, y: 40 });
-        drawMountains(posMountains, 80, 200, "grey", "white");
-        drawMountains(posMountains, 60, 170, "grey", "lightgrey");
-        drawStreet(posStreet);
+        drawSun({ x: 100, y: 75 });
+        drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
+        drawStreet(posStreet, streetWidthBack, streetWidthFront);
+        drawMountains(posMountains, 75, 200, "grey", "white");
+        drawMountains(posMountains, 50, 150, "grey", "lightgrey");
+        drawTrees(8, posTreesStart, posTreesEnd, 0.1, 0.37, 1.4);
+        posTreesStart.x = posStreet.x + streetWidthBack / 2 + treesOffsetBack;
+        posTreesEnd.x = posTreesEnd.x + streetWidthFront + 2 * treesOffsetFront;
         drawTrees(8, posTreesStart, posTreesEnd, 0.1, 0.37, 1.4);
     }
     function drawTrees(_nTrees, _posStart, _posEnd, _minScale, _stepPos, _stepScale) {
+        console.log("Trees", _posStart, _posEnd);
         let transform = crc2.getTransform();
         let step = {
             x: (_posEnd.x - _posStart.x) * _stepPos,
@@ -38,6 +46,7 @@ var L08_Canvas_Alley;
         crc2.setTransform(transform);
     }
     function drawTree() {
+        console.log("Tree");
         let nBranches = 50;
         let maxRadius = 60;
         let branch = new Path2D();
@@ -63,6 +72,7 @@ var L08_Canvas_Alley;
         crc2.restore();
     }
     function drawBackground() {
+        console.log("Background");
         let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
         gradient.addColorStop(0, "lightblue");
         gradient.addColorStop(golden, "white");
@@ -71,41 +81,57 @@ var L08_Canvas_Alley;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     }
     function drawSun(_position) {
-        let r1 = 40;
+        console.log("Sun", _position);
+        let r1 = 30;
         let r2 = 150;
         let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
-        gradient.addColorStop(0, "HSLA(60, 100%, 95%, 1");
-        gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0");
-        let path = new Path2D;
-        path.arc(0, 0, r2, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
+        gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
         crc2.save();
         crc2.translate(_position.x, _position.y);
         crc2.fillStyle = gradient;
-        crc2.fill(path);
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
         crc2.restore();
     }
     function drawCloud(_position, _size) {
+        console.log("Cloud", _position, _size);
         let nParticles = 20;
         let radiusParticle = 50;
-        let particle = new Path2D;
-        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        let particle = new Path2D();
         let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
-        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5");
-        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0");
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
         crc2.save();
         crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
         for (let drawn = 0; drawn < nParticles; drawn++) {
-            let x = (Math.random() - 0.5) * _size.x;
-            let y = -Math.random() * _size.y;
             crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
             crc2.translate(x, y);
-            crc2.fillStyle = gradient;
             crc2.fill(particle);
             crc2.restore();
         }
         crc2.restore();
     }
+    function drawStreet(_position, _widthBack, _widthFront) {
+        console.log("Street", _position, _widthBack, _widthFront);
+        crc2.beginPath();
+        crc2.moveTo(_position.x + _widthBack / 2, _position.y);
+        crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "darkgrey");
+        gradient.addColorStop(0.6, "black ");
+        crc2.fillStyle = gradient;
+        crc2.fill();
+    }
     function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
+        console.log("Mountains", _position, _min, _max);
         let stepMin = 50;
         let stepMax = 150;
         let x = 0;
@@ -127,20 +153,6 @@ var L08_Canvas_Alley;
         crc2.fillStyle = gradient;
         crc2.fill();
         crc2.restore();
-    }
-    function drawStreet(_position) {
-        let fragment = crc2.canvas.width / 8;
-        crc2.beginPath();
-        crc2.moveTo(_position.x + fragment / 2, _position.y);
-        crc2.lineTo(crc2.canvas.width - fragment, crc2.canvas.height);
-        crc2.lineTo(0 + fragment, crc2.canvas.height);
-        crc2.lineTo(_position.x - fragment / 2, _position.y);
-        crc2.closePath();
-        let gradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
-        gradient.addColorStop(0, "darkgrey");
-        gradient.addColorStop(0.6, "black ");
-        crc2.fillStyle = gradient;
-        crc2.fill();
     }
 })(L08_Canvas_Alley || (L08_Canvas_Alley = {}));
 //# sourceMappingURL=Alley.js.map
